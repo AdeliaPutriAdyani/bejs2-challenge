e8ad4e67081fffe62da7da2c8487328dee5b20ba
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-10-19 23:42:38 +0700

    authentication

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,13 @@
 const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const cryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(5);
+
+    return bcrypt.hash(password, salt)
+}
+
 const prisma = new PrismaClient();
 
 module.exports = {
@@ -7,7 +16,7 @@ module.exports = {
             data: {
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password,
+                password: await cryptPassword(req.body.password),
                 profile: {
                     create: {
                         identity_number: req.body.identity_number,
@@ -21,6 +30,46 @@ module.exports = {
         return res.json({
             data:user
         });
+    },
+
+    loginUser: async(req, res) => {
+        const findUser = await prisma.users.findFirst({
+            where: {
+                email: req.body.email
+            }
+        })
+
+        if(!findUser){
+            return res.status(404).json({
+                error: 'User not exists'
+            });
+        }
+
+        if(bcrypt.compareSync(req.body.password, findUser.password)) {
+            const token = jwt.sign({id: findUser.id}, 'secret_key', { expiresIn : '6h'})
+
+            return res.status(200).json({
+                data: {
+                    token
+                }
+            })
+        }
+
+        return res.status(403).json({
+            error: 'Invalid Credentials'
+        });
+    },
+
+    getProfile: async (req, res) => {
+        const user = await prisma.users.findUnique({
+            where: {
+                id: res.user.id
+            }
+        })
+
+        return res.status(200).json({
+            data: user
+        })
     },
 
     getUser: async (req, res) => {
